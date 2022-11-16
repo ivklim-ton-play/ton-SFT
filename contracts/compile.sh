4dbc9656e8aaf54c9e8d10fc885bc2f1d348086d
IvanKlimov <ivan.klimov@beem.me>
2022-11-16 16:53:28 +0700

    update compile.sh

--- contracts/compile.sh
+++ contracts/compile.sh
@@ -1,6 +1,6 @@
-func -SPA -o ./build/sft-wallet.fif stdlib.fc params.fc op-codes.fc sft-utils.fc sft-wallet.fc
-echo '"build/sft-wallet.fif" include 2 boc+>B "build/sft-wallet.boc" B>file' | fift -s
-func -SPA -o ./build/sft-minter.fif stdlib.fc params.fc op-codes.fc sft-utils.fc sft-minter.fc
-func -SPA -o ./build/sft-collection-editable-code.fif stdlib.fc params.fc op-codes.fc sft-collection-editable.fc
+func -SPA -o ./build/jetton-wallet.fif stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-wallet.fc
+echo '"build/jetton-wallet.fif" include 2 boc+>B "build/jetton-wallet.boc" B>file' | fift -s
+func -SPA -o ./build/jetton-minter.fif stdlib.fc params.fc op-codes.fc jetton-utils.fc jetton-minter.fc
+func -SPA -o ./build/jetton-collection-editable-code.fif stdlib.fc params.fc op-codes.fc jetton-collection-editable.fc
 
 fift -s build/print-hex.fif
